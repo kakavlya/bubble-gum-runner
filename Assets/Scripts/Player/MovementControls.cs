ca3a7b8479a54e4d42728edab0c7d4a9d8fba71a
Pavel Ol <megapiharr2008@gmail.com>
2021-11-25 13:49:17 +0200

    Updated code with PuppetMaster

--- Assets/Scripts/Player/MovementControls.cs
+++ Assets/Scripts/Player/MovementControls.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class MovementControls : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _flyingSpeed;
 
     private void Update()
     {
@@ -14,8 +15,9 @@ public class MovementControls : MonoBehaviour
 
     private void ProcessActions()
     {
+        //Vector3 movement = new Vector3(h, 0f, _movementSpeed);
         int horizontal = Mathf.RoundToInt(Input.GetAxis("Horizontal"));
-        Vector3 movement = new Vector3(horizontal, 0f, 0f);
+        Vector3 movement = new Vector3(horizontal, _flyingSpeed, 0f);
         transform.Translate(movement * Time.deltaTime * _speed);
     }
 }
